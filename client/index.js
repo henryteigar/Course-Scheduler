bfd84406a834a9a224c4f0fec51635aa83353fa7
Henry Teigar <henry.teigar@gmail.com>
2017-10-12 23:55:55 +0300

    #9 Fix relative bundle.js path

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
     <BrowserRouter>
-        <switch>
+        <Switch>
             <Route exact path='/' component={App} />
-            <Route path='/test' component={App} />
-        </switch>
+            <Route exact path='/test' component={App} />
+            <Route path='/test/:id' component={App} />
+        </Switch>
     </BrowserRouter>,
 document.getElementById('app'));
