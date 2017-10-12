8dbb053bff377f051a273ba8122c26e2c6e1cca2
Henry Teigar <henry.teigar@gmail.com>
2017-10-12 12:08:55 +0300

    #9 Add basic router

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {name: "test"}
+    }
+
     render() {
-        return <h1>Course-scheduler for UT students.</h1>
+        return (
+            <div>
+
+                <p>{this.state.name}</p>
+                <button>TO</button>
+            </div>
+
+        )
     }
 }
 
--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <BrowserRouter>
+        <switch>
+            <Route exact path='/' component={App} />
+            <Route path='/test' component={App} />
+        </switch>
+    </BrowserRouter>,
+document.getElementById('app'));
