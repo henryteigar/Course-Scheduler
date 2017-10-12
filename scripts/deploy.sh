063a13e38acceb1c8eb2b5531b479d77cc06ea5b
Henry Teigar <henry.teigar@gmail.com>
2017-10-12 10:50:55 +0300

    #4 Modify deployment settings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    cd _site
+    rm -r .git
     git init
 
     git remote add deploy "deploy@course-scheduler.me:/var/www"
     git config user.name "Travis CI"
-    git config user.email "henry.teigar+travisCI@gmail.com"
+    git config user.email "travisCI@gmail.com"
 
     git add .
     git commit -m "Deploy"
