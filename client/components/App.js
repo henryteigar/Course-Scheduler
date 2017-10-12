53e4a8b837e8e9d424a5dd4c687a5e820debd8a3
Henry Teigar <henry.teigar@gmail.com>
2017-10-12 13:03:45 +0300

    #4 Redo CI testing

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
-        return <h1>Course-scheduler for UT students. Test CI</h1>
+        return <h1>Course-scheduler for UT students.</h1>
     }
 }
 
