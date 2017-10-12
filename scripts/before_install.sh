f4017a1813a5d7329354fe3309d475ff7b767be1
Henry Teigar <henry.teigar@gmail.com>
2017-10-13 00:48:03 +0300

    #4 Test CI connection

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_94466d3b2c72_key -iv $encrypted_94466d3b2c72_iv -in deploy-key.enc -out ~\/.ssh/deploy-key -d
+openssl aes-256-cbc -K $encrypted_94466d3b2c72_key -iv $encrypted_94466d3b2c72_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
