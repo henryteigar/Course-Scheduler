f3fa0f1ed7d735ae2d726154d5c8805aa08448f0
Janar Saks <janarsaks@hotmail.com>
2017-10-14 19:01:48 +0300

    #11 added a api endpoint to get a users registered subjects

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { Pool, Client} = require('pg');
+const { Pool } = require('pg');
 
-const connectionString = 'postgresql://postgres:postgres@207.154.242.61:5432/course-scheduler'
+const users = require('./users.js');
+
+const connectionString = 'postgresql://postgres:postgres@207.154.242.61:5432/course-scheduler';
 const pool = new Pool({
     connectionString: connectionString,
 });
@@ -11,29 +13,17 @@ router.get('/', (req, res) => {
     res.send("This is our API")
 });
 
+router.use('/users', users);
+
 router.get('/subjects', (req, res) => {
-    var param = req.query.query;
-    var whereQuery = "";
+    let param = req.query.query;
+    let whereQuery = "";
+
     if (param) {
         whereQuery = " WHERE LOWER(title) LIKE '%" + param +"%'";
-    };
-
-    pool.query('SELECT * FROM SUBJECTS' + whereQuery, (err, result) => {
-        if (err) {
-            return console.log('ERROR ', err);
-        }
-        res.send(result.rows);
-    });
-});
-
-router.get('/users/:id', (req, res) => {
-    var id = req.params.id;
-    var whereQuery = "";
-    if (id) {
-        whereQuery = " WHERE id = " + id;
     }
 
-    pool.query('SELECT * FROM USERS' + whereQuery, (err, result) => {
+    pool.query('SELECT * FROM SUBJECTS' + whereQuery, (err, result) => {
         if (err) {
             return console.log('ERROR ', err);
         }
---
+++ server/routes/users.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const { Pool } = require('pg');
+
+const connectionString = 'postgresql://postgres:postgres@207.154.242.61:5432/course-scheduler';
+const pool = new Pool({
+    connectionString: connectionString,
+});
+
+router.get('/', (req, res) => {
+
+    pool.query('SELECT * FROM USERS', (err, result) => {
+        if (err) {
+            return console.log('ERROR ', err);
+        }
+        res.send(result.rows);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+
+    pool.query('SELECT * FROM USERS WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            return console.log('ERROR ', err);
+        }
+        res.send(result.rows);
+    });
+});
+
+router.get('/:id/registered_subjects', (req, res) => {
+    let id = req.params.id;
+
+    pool.query('SELECT * FROM USER_REGISTERED_SUBJECT WHERE user_id = $1', [id], (err, result) => {
+        if (err) {
+            return console.log('ERROR ', err);
+        }
+        res.send(result.rows);
+    });
+});
+
+module.exports = router;
