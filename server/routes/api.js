3a15800ab02f1ed4c0725373aa9c4f04862f9853
Janar Saks <janarsaks@hotmail.com>
2017-10-14 14:47:42 +0300

    #11 added a endpoint for all subjects

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { Pool, Client} = require('pg');
+
+const connectionString = 'postgresql://postgres:postgres@207.154.242.61:5432/course-scheduler'
+const pool = new Pool({
+    connectionString: connectionString,
+});
 
 router.get('/', (req, res) => {
     res.send("This is our API")
 });
 
-router.get('/janar', (req, res) => {
-    res.send("yo")
+router.get('/subjects', (req, res) => {
+    pool.query('select * from subjects', (err, result) => {
+        if (err) {
+            return console.log('ERROR ', err)
+        }
+        res.send(result.rows)
+    })
 });
 
 module.exports.router = router;
