8c7b6527fb67e9bfc947e4917aa18fc78318c92f
Miron <miron.storozhev96@gmail.com>
2017-10-31 22:48:39 +0200

    #48 Add initial LoginContainer

---
+++ client/containers/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import LoginContainer from './LoginContainer/LoginContainer';
+
+
+import '../css/main.scss';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <LoginContainer/>
+            </div>
+        )
+    }
+}
+
+export default Login;
