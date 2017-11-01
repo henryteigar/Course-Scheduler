bdd4324ae759debe7cf883dec72fac78855f8f92
Miron <miron.storozhev96@gmail.com>
2017-11-01 17:09:24 +0200

    #48 Add text to LoginBoard

--- client/containers/LoginContainer/InputfieldContainer/LoginBoard.js
+++ client/containers/LoginContainer/InputfieldContainer/LoginBoard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import 'client/containers/LoginContainer/InputfieldContainer/loginboard-container.scss';
+import 'client/containers/LoginContainer/InputfieldContainer/loginboard.scss';
 import Button from "client/components/Button/Button";
 
 class InputfieldContainer extends Component {
     render() {
         return (
             <div className="loginBoard">
+                <p>Sign in with your UT account</p>
                 <div className="logInButton">
                     <Button class="green_login" name="Sign in"/>
                 </div>
