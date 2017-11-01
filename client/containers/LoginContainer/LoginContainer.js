26b2aa32b2d961a82aae4fd1d6caafcbe917dff5
Miron <miron.storozhev96@gmail.com>
2017-11-01 16:12:57 +0200

    #48 Add Logo and title to LoginContainer

--- client/containers/LoginContainer/LoginContainer.js
+++ client/containers/LoginContainer/LoginContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import 'client/containers/LoginContainer/log-in-container.scss';
+import 'client/containers/LoginContainer/login-container.scss';
+import InputfieldContainer from 'client/containers/LoginContainer/InputfieldContainer/InputfieldContainer';
 
 class LoginContainer extends Component {
     render() {
         return (
             <div className="loginContainer">
+                <img className="logo" src="../../images/ut-logo.svg" />
+                <p>University of Tartu Course Scheduler</p>
+                <InputfieldContainer/>
             </div>
         )
     }
