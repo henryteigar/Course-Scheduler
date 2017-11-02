6f6a0b1f20e255867d49559f1b0071ee6b271b23
Miron <miron.storozhev96@gmail.com>
2017-11-02 21:26:26 +0200

    #48 Correct input fields design

--- client/containers/LoginContainer/InputfieldContainer/LoginBoard.js
+++ client/containers/LoginContainer/InputfieldContainer/LoginBoard.js
@@ -8,22 +8,22 @@ class InputfieldContainer extends Component {
         return (
             <div className="loginBoard">
                 <p>Sign in with your UT account</p>
-                <div className="userName_input">
+                <div className="input userName_input">
                     <InputField
-                        class="userName_input_field"
+                        class="input_field"
                         type="text"
                         placeholder="Username"
                     />
                 </div>
-                <div className="passWord_input">
+                <div className="input passWord_input">
                     <InputField
-                        class="password_input_field"
+                        class="input_field"
                         type="password"
                         placeholder="Password"
                     />
                 </div>
                 <div className="logInButton">
-                    <Button class="green_login" name="Sign in"/>
+                    <Button class="big green" name="Sign in"/>
                 </div>
             </div>
         )
