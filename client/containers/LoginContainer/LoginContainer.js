02a4a798d58d293c82ee89bfa4cf032e028f7de9
Henry Teigar <henry.teigar@gmail.com>
2017-11-09 07:58:25 +0200

    #48 Fix little bugs with login box and restructure code a bit

--- client/containers/LoginContainer/LoginContainer.js
+++ client/containers/LoginContainer/LoginContainer.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'client/containers/LoginContainer/login-container.scss';
-import InputfieldContainer from 'client/containers/LoginContainer/LoginBoardContainer/LoginBoard';
+import LoginFieldsContainer from 'client/containers/LoginContainer/LoginFieldsContainer/LoginFieldsContainer';
 
 class LoginContainer extends Component {
     render() {
         return (
             <div className="loginContainer">
-                <img className="logo" src="../../images/ut-logo.svg" />
-                <p>University of Tartu Course Scheduler</p>
-                <InputfieldContainer/>
+                <img src="../../images/ut-logo.svg"/>
+                <h1>University of Tartu Course Scheduler</h1>
+                <LoginFieldsContainer/>
             </div>
         )
     }
 }
+
 export default LoginContainer;
