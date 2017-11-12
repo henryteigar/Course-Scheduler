e8b49b3bd336e1d6dd1958f41b440e28110aa015
Janar Saks <janarsaks@hotmail.com>
2017-11-12 22:20:15 +0200

    #61 Refactor

--- mockServerOIS1/db/views/v_courses.sql
+++ mockServerOIS1/db/views/v_courses.sql
@@ -18,8 +18,16 @@ CREATE OR REPLACE VIEW ois1.v_courses AS
             courses.limit_of_attendants AS limit_of_attendants,
             courses.registered_attendants AS registered_attendants,
             (SELECT ARRAY_AGG(ROW_TO_JSON(obj1)) FROM (SELECT occurence.type AS type,
-                              (SELECT ROW_TO_JSON(obj) FROM (SELECT times.id AS id, times.week AS week, times.day AS day, times.start_time AS start_time,
-                                                                        times.end_time AS end_time FROM ois1.occurrence_time times WHERE
+                              (SELECT ROW_TO_JSON(obj) FROM (SELECT times.id AS id,
+                                                                    times.week AS week,
+                                                                    times.day AS day,
+                                                                    times.start_time AS start_time,
+                                                                    times.end_time AS end_time,
+                                                                    to_char(times.start_time, 'HH')::int AS start_hour,
+                                                                    to_char(times.start_time, 'MM')::int AS start_minute,
+                                                                    to_char(times.end_time, 'HH')::int AS end_hour,
+                                                                    to_char(times.end_time, 'MM')::int AS end_minute
+                                                               FROM ois1.occurrence_time times WHERE
                                                                         times.id = occurence.time_id) obj) AS time,
                               occurence.schedule_id AS schedule,
                               occurence.remarks AS remarks,
@@ -33,4 +41,4 @@ CREATE OR REPLACE VIEW ois1.v_courses AS
 
                        FROM ois1.occurrences occurence
                        JOIN ois1.course_occurrences cooc ON cooc.occurrence_id = occurence.id AND cooc.course_id = courses.id) obj1) AS occurrences
-     FROM ois1.courses courses
+     FROM ois1.courses courses;
