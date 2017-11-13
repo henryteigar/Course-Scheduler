d66773db3fccd1da00b81740e45a1ad8d6865e33
Miron <miron.storozhev96@gmail.com>
2017-11-13 05:00:08 +0200

    #79 Add tests for registered-courses endpoint

--- test/tests.js
+++ test/tests.js
@@ -13,9 +13,8 @@ describe("Test 1. -> Testing courses API endpoint",function(){
         server
             .get("course-scheduler.me:3000/api/courses")
             .expect("Content-type",/json/)
-            .expect(200) // THis is HTTP response
+            .expect(200)
             .end(function(err,res){
-                // HTTP status should be 200
                 assert.equal(res.status, 200);
                 done();
             });
@@ -23,7 +22,21 @@ describe("Test 1. -> Testing courses API endpoint",function(){
 
 });
 
-describe('Test 2. -> Testing drafts API endpoint', function() {
+describe("Test 2. -> Testing drafts API endpoint",function(){
+    it("Should get response from /drafts",function(done){
+        server
+            .get("course-scheduler.me:3000/api/drafts")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
+
+describe('Test 3. -> Testing drafts API endpoint', function() {
     it('Adding and removing courses from draft and comparing response sizes.', function(done) {
 
         let length = -1;
@@ -65,5 +78,66 @@ describe('Test 2. -> Testing drafts API endpoint', function() {
                 });
         }
         });
+
+});
+
+describe("Test 4. -> Testing registered-courses API endpoint",function(){
+    it("Should get response from /registered-courses",function(done){
+        server
+            .get("course-scheduler.me:3000/api/registered-courses")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
+
+
+
+describe('Test 5. -> Testing registered-courses API endpoint', function() {
+    it('Adding and removing courses from registered-courses and comparing response sizes.', function(done) {
+
+        let length = -1;
+        let contains = false;
+        server.get("/api/registered-courses")
+            .end(function(err,res){
+                let body = res.body;
+                contains = (body.toString()).indexOf("Java Harjutused") === -1;
+                length = Object.keys(res.body).length;
+                done();
+            });
+
+        if (contains){
+            request.delete('http://course-scheduler.me:3000/api/registered-courses/8');
+            server
+                .get("/api/registered-courses")
+                .expect("Content-type",/json/)
+                .expect(200)
+                .end(function(err,res){
+                    assert.equal(res.status, 200);
+                    assert.equal(Object.keys(res.body).length, (length - 1));
+                    request.post('http://course-scheduler.me:3000/api/registered-courses/8');
+                    done();
+                });
+        }
+        else {
+            request.post('http://course-scheduler.me:3000/api/registered-courses/8');
+
+            server
+                .get("/api/registered-courses")
+                .expect("Content-type", /json/)
+                .expect(200)
+                .end(function (err, res) {
+                    // HTTP status should be 200
+                    assert.equal(res.status, 200);
+                    assert.equal(Object.keys(res.body).length, (length + 1));
+                    request.delete('http://course-scheduler.me:3000/api/registered-courses/8');
+                    done();
+                });
+        }
+    });
 
 });
