531fac062b0b4a36d9bc905ce87e240b3ceb3987
Miron <miron.storozhev96@gmail.com>
2017-11-13 02:26:32 +0200

    #80 add test for draft timetable endpoint

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,17 @@
 const assert = require('assert');
 //const chai = require('chai');
-//const request = require('request');
+const request = require('request');
 const supertest = require("supertest");
+//const expect = chai.expect();
 //const express = require('express');
 //const router = express.Router();
 
 const server = supertest.agent("course-scheduler.me:3000");
 
 describe("Test 1. -> Testing courses API endpoint",function(){
-    it("Should GET /courses",function(done){
+    it("Should get response from /courses",function(done){
         server
-            .get("/api/courses")
+            .get("course-scheduler.me:3000/api/courses")
             .expect("Content-type",/json/)
             .expect(200) // THis is HTTP response
             .end(function(err,res){
@@ -21,23 +22,35 @@ describe("Test 1. -> Testing courses API endpoint",function(){
     });
 
 });
-/*
-describe('Test 2. -> Adding courses to drafts', function() {
-    it('checks adding course to drafts', function(done) {
-        request.post('/drafts/:8');
+
+describe('Test 2. -> Testing drafts API endpoint', function() {
+    it('adding courses to draft and comparing response size. Should be bigger by 1', function(done) {
+
+        let length = 0;
+        server
+            .get("/api/drafts")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                // HTTP status should be 200
+                length = Object.keys(res.body).length;
+                done();
+            });
+
+        request.post('http://course-scheduler.me:3000/api/drafts/8');
 
         server
             .get("/api/drafts")
             .expect("Content-type",/json/)
-            .expect(200) // THis is HTTP response
+            .expect(200)
             .end(function(err,res){
                 // HTTP status should be 200
                 assert.equal(res.status, 200);
+                assert.equal(Object.keys(res.body).length, (length + 1));
                 done();
             });
-
+        request.delete('http://course-scheduler.me:3000/api/drafts/8');
     });
 
 });
 
-*/
