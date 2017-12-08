ff4fce451b5a0d031e086c53495b51cec616b71d
Henry Teigar <henry.teigar@gmail.com>
2017-12-08 22:13:11 +0200

    #106 Complete logout modal css

--- client/components/Profile/DetailedProfileBox/DetailedProfileBox.js
+++ client/components/Profile/DetailedProfileBox/DetailedProfileBox.js
@@ -7,6 +7,15 @@ const DetailedProfileBox = (props) => {
     return (
         <div className="detailed-profile-box">
             <img src="../../images/user4.svg"/>
+            <div className="info-text">
+                <p className="name">John Doe</p>
+                <p className="email">john@doe.ut.ee</p>
+                <p className="student-id">B33043</p>
+            </div>
+            <div className="buttons">
+                <p>Settings</p>
+                <p>Sign out</p>
+            </div>
         </div>
     )
 };
