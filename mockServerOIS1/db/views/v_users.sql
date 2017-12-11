51da0b1dc239fe59172c99943a0da93253c51c3a
Janar Saks <janarsaks@hotmail.com>
2017-12-11 02:39:52 +0200

    #61 Add credits to users view

--- mockServerOIS1/db/views/v_users.sql
+++ mockServerOIS1/db/views/v_users.sql
@@ -12,5 +12,18 @@ CREATE OR REPLACE VIEW ois1.v_users AS
             (SELECT row_to_json(obj) FROM
                                 (SELECT * FROM ois1.schedule WHERE schedule.id = users.schedule_id) obj) AS schedule,
             (SELECT row_to_json(obj) FROM
-                                 (SELECT * FROM ois1.level_of_studies WHERE level_of_studies.id = users.level_of_study_id) obj) AS level_of_study
-     FROM ois1.users users;
+                                 (SELECT * FROM ois1.level_of_studies WHERE level_of_studies.id = users.level_of_study_id) obj) AS level_of_study,
+            COALESCE((SELECT SUM(ois1.courses.credits) FROM ois1.courses JOIN ois1.registered_courses ON ois1.courses.id = ois1.registered_courses.course_id
+                                JOIN ois1.course_curricula ON ois1.registered_courses.course_id = ois1.course_curricula.course_id
+                                WHERE ois1.registered_courses.user_id = users.id AND ois1.course_curricula.curricula_id = users.curricula_id
+                                AND ois1.course_curricula.course_type_id = 1), 0) AS obligatory_credits,
+            COALESCE((SELECT SUM(ois1.courses.credits) FROM ois1.courses JOIN ois1.registered_courses ON ois1.courses.id = ois1.registered_courses.course_id
+                                JOIN ois1.course_curricula ON ois1.registered_courses.course_id = ois1.course_curricula.course_id
+                                WHERE ois1.registered_courses.user_id = users.id AND ois1.course_curricula.curricula_id = users.curricula_id
+                                AND ois1.course_curricula.course_type_id = 2), 0) AS elective_credits,
+            COALESCE((SELECT SUM(ois1.courses.credits) FROM ois1.courses JOIN ois1.registered_courses ON ois1.courses.id = ois1.registered_courses.course_id
+                                WHERE ois1.registered_courses.user_id = users.id
+                                AND NOT EXISTS(SELECT 1 FROM ois1.course_curricula WHERE ois1.course_curricula.course_id = ois1.courses.id
+                                AND ois1.course_curricula.curricula_id = users.curricula_id)), 0) AS optional_credits
+
+     FROM ois1.users users GROUP BY id
