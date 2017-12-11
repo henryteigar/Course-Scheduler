cdb4ee427fd6b8e7da12473dcb22b40e96dc6b2f
Miron <miron.storozhev96@gmail.com>
2017-12-11 06:55:08 +0200

    #80 Fix new templates for API tests

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,16 @@
 const assert = require('chai').assert;
 //const chai = require('chai');
 //const request = require('request');
-//const supertest = require("supertest");
+const supertest = require("supertest");
 //const expect = chai.expect();
 //const express = require('express');
 //const router = express.Router();
 const axios = require('axios');
 
-//const server = supertest.agent("course-scheduler.me:3000");
+const server = supertest.agent("course-scheduler.me:3000");
 //const remoteApiUrl = "course-scheduler.me:3000/api";
 
-
+let jwt;
 describe("Test 1. -> Testing login",function(){
     it("Should get JWT from /api/login",function(done){
 
@@ -21,6 +21,7 @@ describe("Test 1. -> Testing login",function(){
             //console.log(response.data);
             assert.equal(response.status, 200);
             assert.equal(response.data.jwt.length > 0, true);
+            jwt = response.data.jwt;
             done();
         }).catch(function (error) {
            done(error);
@@ -28,28 +29,16 @@ describe("Test 1. -> Testing login",function(){
     });
 });
 
+/*
 describe("Test 2. -> Testing courses API endpoint", function(){
-    let jwt;
-
-    before(function () {
-        axios.post('http://course-scheduler.me:3000/api/login' , {
-            username: "test",
-            password: "test"}
-        ).then(function (response) {
-            jwt = response.data.jwt
-        }).catch(function (error) {
-            done(error);
-        });
-    });
 
     it("Should get courses from /api/courses",function (done) {
         this.timeout(10000);
         let instance = axios.create({
             headers: {'session-key': jwt}
         });
-
-
-        axios.get('http://course-scheduler.me:3000/api/courses' ,{}, instance
+        console.log(instance)
+        axios.get('http://course-scheduler.me:3000/api/courses' , instance
         ).then(function (response) {
             assert.equal(response.status, 200);
             done();
@@ -81,7 +70,7 @@ describe("Test 3. -> Testing drafts API endpoint", function(){
         });
 
 
-        axios.get('http://course-scheduler.me:3000/api/drafts' ,{}, instance
+        axios.get('http://course-scheduler.me:3000/api/drafts' , instance
         ).then(function (response) {
             assert.equal(response.status, 200);
             done();
@@ -207,7 +196,7 @@ describe("Test 7. -> Testing user API endpoint", function(){
         });
 
 
-        axios.get('http://course-scheduler.me:3000/api/user' ,{}, instance
+        axios.get('http://course-scheduler.me:3000/api/user' , instance
         ).then(function (response) {
             assert.equal(response.status, 200);
             done();
@@ -218,8 +207,21 @@ describe("Test 7. -> Testing user API endpoint", function(){
     });
 
 });
+*/
 
-/*
+describe("Test 1. -> Testing courses API endpoint",function(){
+    it("Should get response from /courses",function(done){
+        server
+            .get("course-scheduler.me:3000/api/courses")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
 
 describe("Test 2. -> Testing drafts API endpoint",function(){
     it("Should get response from /drafts",function(done){
@@ -235,6 +237,85 @@ describe("Test 2. -> Testing drafts API endpoint",function(){
 
 });
 
+describe("Test 3. -> Testing drafts API endpoint",function(){
+    it("Adding courses to draft and comparing response sizes.",function(done){
+        server
+            .get("course-scheduler.me:3000/api/drafts")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+    it("Removing courses from draft and comparing response sizes.",function(done){
+        server
+            .get("course-scheduler.me:3000/api/drafts")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
+
+describe("Test 4. -> Testing registered-courses API endpoint",function(){
+    it("Should get response from /registered-courses",function(done){
+        server
+            .get("course-scheduler.me:3000/api/drafts")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
+
+describe("Test 5. -> Testing registered-courses API endpoint",function(){
+    it("Adding courses to registered-courses and comparing response sizes.",function(done){
+        server
+            .get("course-scheduler.me:3000/api/registered-courses")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+    it("Removing courses from registered-courses and comparing response sizes.",function(done){
+        server
+            .get("course-scheduler.me:3000/api/registered-courses")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
+
+describe("Test 6. -> Testing user API endpoint",function(){
+    it("Should get response from /user",function(done){
+        server
+            .get("course-scheduler.me:3000/api/user")
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                assert.equal(res.status, 200);
+                done();
+            });
+    });
+
+});
+
+/*
 describe("Test 3. -> Testing drafts API endpoint by adding and removing courses",function(){
 
     let length = -1;
